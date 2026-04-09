8d4f11067fbdb0b1317a497ad13e089afb094c15
Moaz Sayed <moaz.sayed.said@gmail.com>
2026-04-09 12:43:20 +0200

    feat(config): update Vite configuration to use environment variables for API URL

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default ({ mode }: { mode: string }) => {
+  // يحمل متغيرات البيئة حسب الـ mode (development / production)
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL, // هنا استخدمنا المتغير
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
-});
+  });
+};
